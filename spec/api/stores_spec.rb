d134356e504b0fac77fcf879657709a7bda95c1d
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-03-01 15:27:23 -0300

    feat: configura rota listing e adiciona controlador para produto

--- spec/api/stores_spec.rb
+++ spec/api/stores_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe"/stores",type: :request do
     describe"GET/show" do
         it "renders a successful response with stores data" do
-            store = Store.create! name: "New Store"
+            store = end
             get "/stores/#{store.id}", headers:{"Accept" => "application/json"}
             json = JSON.parse(response.body)
 
