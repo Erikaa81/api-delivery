bb143a6fe38fd4c9a9bea7f4d5d5f1584602ca0a
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-03-20 12:11:49 -0300

    test: Corrige spec de api

--- spec/api/stores_spec.rb
+++ spec/api/stores_spec.rb
@@ -1,9 +1,31 @@
 require "rails_helper"
 
 RSpec.describe"/stores",type: :request do
+
+    let(:user){
+        user = User.new(
+          email: "user@example.com",
+          password:"123456", 
+          password_confirmation: "123456"
+        )
+        user.save!
+        user
+       }
+       
+        let(:valid_attributes) {
+          {name:"New Store", user: user}
+        }
+      
+        let(:invalid_attributes) {
+          {name:nil}
+        }
+        before{
+          sign_in(user)
+        }
+    
     describe"GET/show" do
         it "renders a successful response with stores data" do
-            store = end
+            store = Store.create! valid_attributes
             get "/stores/#{store.id}", headers:{"Accept" => "application/json"}
             json = JSON.parse(response.body)
 
