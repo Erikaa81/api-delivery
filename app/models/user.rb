f3b35eda09dc67822bbb5cdf3dee65049e17f282
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-04-05 10:36:56 -0300

    feat: Altera para nao consultar o banco de dados a cada request e adiciona expiração de Token (jwt) com tratamento de exceções

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,17 @@ class User < ApplicationRecord
   def self.token_for(user)
     jwt_headers = {exp: 15.minute.from_now.to_i}
     payload = {id: user.id, email: user.email, role: user.role}
-    JWT.encode(payload.merge(jwt_headers),"muito.secreto", "HS256")
+  
+    JWT.encode payload.merge(jwt_headers), Rails.application.credentials.jwt_secret_key, "HS256"
   end
 
   def self.from_token(token)
     decoded = JWT.decode(
-    token, "muito.secreto", true, {algorithm: "HS256"}
+    token,  Rails.application.credentials.jwt_secret_key, true, {algorithm: "HS256"}
     )
-    user_data = decoded[0].with_indifferent_access
-    User.find(user_data[:id])
+    user_data = decoded[0].with_indifferent_access.except(:exp)
+    User.new(user_data)
+    
   rescue JWT::DecodeError => e
     Rails.logger.error "erro ao decodificar token JWT: #{e.message}"
     raise InvalidToken.new 
