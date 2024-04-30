f3993504e5decdac3cb684bcaebf8f73184a3df5
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-04-30 19:59:06 -0300

    feat: Garante que uma loja sempre pertença a um seller

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,13 @@
 class Store < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true, length:{minimum:3}
-end
+    before_validation :ensure_seller
+  
+    validates :name, presence: true, length: { minimum: 3 }
+  
+    private
+  
+    def ensure_seller
+      self.user = nil if self.user && !self.user.seller?
+    end
+  end
+  
