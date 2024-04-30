6883a26d7989607252316d9430b26006fc97b419
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-04-30 20:01:08 -0300

    feat: Adiciona um helper para facilitar autenticação via Token e Credential

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,54 +25,74 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
-  ActiveRecord::Migration.maintain_test_schema!
+ ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
+ abort e.to_s.strip
 end
+
+module APIRequestHelpers
+def api_sign_in(user, credential)
+ post(
+   "/sign_in",
+   headers:{
+     "Accept" => "application/json", 
+     "X-API-KEY" => credential.key
+},
+params:{
+ login:{
+   email: user.email,
+   password: user.password
+ }
+}
+ )
+ JSON.parse(response.body)
+end
+end
+
+
 RSpec.configure do |config|
 
-  config.include(
-       Devise::Test::IntegrationHelpers, type: :request
-    )
+ config.include Devise::Test::IntegrationHelpers, type: :request
+ config.include APIRequestHelpers, type: :request
+   
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
-  ]
+ # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+ config.fixture_paths = [
+   Rails.root.join('spec/fixtures')
+ ]
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+ # If you're not using ActiveRecord, or you'd prefer not to run each of your
+ # examples within a transaction, remove the following line or assign false
+ # instead of true.
+ config.use_transactional_fixtures = true
 
- 
 
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
+ # You can uncomment this line to turn off ActiveRecord support entirely.
+ # config.use_active_record = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://rspec.info/features/6-0/rspec-rails
-  config.infer_spec_type_from_file_location!
+ # RSpec Rails can automatically mix in different behaviours to your tests
+ # based on their file location, for example enabling you to call `get` and
+ # `post` in specs under `spec/controllers`.
+ #
+ # You can disable this behaviour by removing the line below, and instead
+ # explicitly tag your specs with their type, e.g.:
+ #
+ #     RSpec.describe UsersController, type: :controller do
+ #       # ...
+ #     end
+ #
+ # The different available types are documented in the features, such as in
+ # https://rspec.info/features/6-0/rspec-rails
+ config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+ # Filter lines from Rails gems in backtraces.
+ config.filter_rails_from_backtrace!
+ # arbitrary gems may also be filtered via:
+ # config.filter_gems_from_backtrace("gem name")
 end
 Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
+ config.integrate do |with|
+   with.test_framework :rspec
+   with.library :rails
+ end
 end
