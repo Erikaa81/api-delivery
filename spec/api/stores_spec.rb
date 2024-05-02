72355682a33a9f6297ab1b8f1283207485d5fa52
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-05-02 12:18:38 -0300

    feat: Atualiza o teste de API que vai precisar da nova autenticação

--- spec/api/stores_spec.rb
+++ spec/api/stores_spec.rb
@@ -1,35 +1,42 @@
 require "rails_helper"
 
 RSpec.describe"/stores",type: :request do
-
-    let(:user){
-        user = User.new(
-          email: "user@example.com",
-          password:"123456", 
-          password_confirmation: "123456",
-          role: :seller        )
-        user.save!
-        user
+   let(:user){
+       user = User.new(
+         email: "usuario@example.com",
+         password:"123456", 
+         password_confirmation: "123456",
+         role: :seller        )
+       user.save!
+       user
+      }
+           
+       let(:valid_attributes) {
+         {name:"New Store", user: user}
        }
-       
-        let(:valid_attributes) {
-          {name:"New Store", user: user}
-        }
-      
-        let(:invalid_attributes) {
-          {name:nil}
-        }
-        before{
-          sign_in(user)
-        }
-    
-    describe"GET/show" do
-        it "renders a successful response with stores data" do
-            store = Store.create! valid_attributes
-            get "/stores/#{store.id}", headers:{"Accept" => "application/json"}
-            json = JSON.parse(response.body)
+     
+       let(:invalid_attributes) {
+         {name:nil}
+       }
+       before{
+         sign_in(user)
+       }
 
-            expect(json["name"]).to eq "New Store"
+       let(:credential) {Credential.create_access(:seller)}
+       let(:signed_in) { api_sign_in(user, credential)}
+
+      describe "GET /show" do
+        it "renders a successful response with store data" do
+          store = Store.create!(name: "New Store", user: user)
+          get("/stores/#{store.id}",
+            headers: {
+              "Accept" => "application/json",
+              "Authorization" => "Bearer #{signed_in["token"]}"
+            }
+          )
+          json = JSON.parse(response.body)
+      
+          expect(json["name"]).to eq "New Store"
         end
+      end
     end
-end
