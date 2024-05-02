2fc69b26eccbf7d17a9363b088866cab7ed6abfc
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-05-02 12:30:10 -0300

    refactor: Atualiza função create_access

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -2,7 +2,7 @@ class Credential < ApplicationRecord
   enum access: [:seller, :buyer]
 
   def self.create_access(access)
-    raise "Invalid Access" unless Credential.accesses.keys.map(&:to_sym).include?(access)
+    raise "Invalid Access" if !Credential.accesses.keys.map(&:to_sym).include?(access)
     create!(access: access, key: SecureRandom.base64(20))
   end
 end
