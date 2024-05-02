b3ee71a8dacbbfe6cdb235c7aa032d8d4b0c72a6
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-05-02 12:28:01 -0300

    feat: Altera a implementação para garantir os 'direitos' do usuario admin

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,30 +1,36 @@
 class StoresController < ApplicationController
   before_action :authenticate! 
   before_action :set_store, only: %i[ show edit update destroy ]
-
+ 
   # GET /stores or /stores.json
-  def index
-    @stores = Store.all
+  def index 
+    if current_user.admin?
+      @stores = Store.all
+    else
+      @stores = current_user.stores
+    end
   end
-
+   
   # GET /stores/1 or /stores/1.json
   def show
   end
-
+ 
   # GET /stores/new
   def new
     @store = Store.new
   end
-
+ 
   # GET /stores/1/edit
   def edit
   end
-
+ 
   # POST /stores or /stores.json
   def create
     @store = Store.new(store_params)
+    if !current_user.admin?
     @store.user = current_user
-
+    end
+ 
     respond_to do |format|
       if @store.save
         format.html { redirect_to store_url(@store), notice: "Store was successfully created." }
@@ -35,7 +41,7 @@ class StoresController < ApplicationController
       end
     end
   end
-
+ 
   # PATCH/PUT /stores/1 or /stores/1.json
   def update
     respond_to do |format|
@@ -48,25 +54,30 @@ class StoresController < ApplicationController
       end
     end
   end
-
+ 
   # DELETE /stores/1 or /stores/1.json
   def destroy
     @store.destroy!
-
+ 
     respond_to do |format|
       format.html { redirect_to stores_url, notice: "Store was successfully destroyed." }
       format.json { head :no_content }
     end
   end
-
+ 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_store
       @store = Store.find(params[:id])
     end
-
+ 
     # Only allow a list of trusted parameters through.
     def store_params
-      params.require(:store).permit(:name)
+      required = params.require(:store)
+      if current_user.admin?
+        required.permit(:name, :user_id)
+      else
+        required.permit(:name)
     end
-end
+  end
+ end
