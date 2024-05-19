2dceb59c8972a0bd870dd1eab76b029cfe8e9f07
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-05-19 14:20:28 -0300

    feat: Permite que usario 'admin' crie lojas, desde que a loja seja de um usuario do tipo 'seller'

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,39 +1,52 @@
 class StoresController < ApplicationController
-  before_action :authenticate! 
-  before_action :set_store, only: %i[ show edit update destroy ]
- 
+  skip_forgery_protection only: [ :create, :update, :destroy ]
+  before_action :authenticate!
+  before_action :set_store, only: %i[show edit update destroy]
+  rescue_from User::InvalidToken, with: :not_authorized
+
+
   # GET /stores or /stores.json
   def index 
-    if current_user.admin?
-      @stores = Store.all
+    if current_user.present?
+      if current_user.admin?
+        @stores = Store.all
+      else
+        @stores = current_user.stores
+      end
     else
-      @stores = current_user.stores
+      render json: { error: "Você deve estar logado para visualizar esta página." }, status: :unauthorized
     end
   end
-   
+
   # GET /stores/1 or /stores/1.json
   def show
   end
- 
+
   # GET /stores/new
   def new
     @store = Store.new
+    if current_user.admin?
+      @sellers = User.where(role: :seller)
+    end
   end
- 
+
   # GET /stores/1/edit
   def edit
+    if current_user.admin?
+      @sellers = User.where(role: :seller)
+    end
   end
- 
+
   # POST /stores or /stores.json
   def create
     @store = Store.new(store_params)
     if !current_user.admin?
-    @store.user = current_user
+      @store.user = current_user
     end
- 
+
     respond_to do |format|
       if @store.save
-        format.html { redirect_to store_url(@store), notice: "Store was successfully created." }
+        format.html { redirect_to store_url(@store), notice: "Loja criada com sucesso." }
         format.json { render :show, status: :created, location: @store }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,12 +54,13 @@ class StoresController < ApplicationController
       end
     end
   end
- 
+  
+
   # PATCH/PUT /stores/1 or /stores/1.json
   def update
     respond_to do |format|
       if @store.update(store_params)
-        format.html { redirect_to store_url(@store), notice: "Store was successfully updated." }
+        format.html { redirect_to store_url(@store), notice: "Loja atualizada com sucesso." }
         format.json { render :show, status: :ok, location: @store }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,23 +68,23 @@ class StoresController < ApplicationController
       end
     end
   end
- 
-  # DELETE /stores/1 or /stores/1.json
+
+
   def destroy
     @store.destroy!
- 
+
     respond_to do |format|
-      format.html { redirect_to stores_url, notice: "Store was successfully destroyed." }
+      format.html { redirect_to stores_url, notice: "Loja excluída com sucesso." }
       format.json { head :no_content }
     end
   end
- 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_store
       @store = Store.find(params[:id])
     end
- 
+
     # Only allow a list of trusted parameters through.
     def store_params
       required = params.require(:store)
@@ -78,6 +92,6 @@ class StoresController < ApplicationController
         required.permit(:name, :user_id)
       else
         required.permit(:name)
+      end
     end
-  end
- end
+end
