e96e67d044f37e7010843d7fb18b46d5678b6fea
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-05-30 13:05:53 -0300

    feat: Adiciona soft delete e imagem a loja, e melhora a formatação

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,48 +1,41 @@
 class StoresController < ApplicationController
-  skip_forgery_protection only: [ :create, :update, :destroy ]
+  skip_forgery_protection only: [:create, :update, :destroy]
   before_action :authenticate!
   before_action :set_store, only: %i[show edit update destroy]
   rescue_from User::InvalidToken, with: :not_authorized
 
-
-  # GET /stores or /stores.json
-  def index 
+  def index
     if current_user.present?
       if current_user.admin?
         @stores = Store.all
       else
         @stores = current_user.stores
       end
+      respond_to do |format|
+        format.html
+        format.json { render json: @stores.map { |store| store.as_json(methods: :image_url) } }
+      end
     else
       render json: { error: "Você deve estar logado para visualizar esta página." }, status: :unauthorized
     end
   end
 
-  # GET /stores/1 or /stores/1.json
   def show
+    render json: @store.as_json(methods: :image_url), status: :ok
   end
 
-  # GET /stores/new
   def new
     @store = Store.new
-    if current_user.admin?
-      @sellers = User.where(role: :seller)
-    end
+    @sellers = User.where(role: :seller) if current_user.admin?
   end
 
-  # GET /stores/1/edit
   def edit
-    if current_user.admin?
-      @sellers = User.where(role: :seller)
-    end
+    @sellers = User.where(role: :seller) if current_user.admin?
   end
 
-  # POST /stores or /stores.json
   def create
     @store = Store.new(store_params)
-    if !current_user.admin?
-      @store.user = current_user
-    end
+    @store.user = current_user unless current_user.admin?
 
     respond_to do |format|
       if @store.save
@@ -54,44 +47,42 @@ class StoresController < ApplicationController
       end
     end
   end
-  
 
-  # PATCH/PUT /stores/1 or /stores/1.json
   def update
-    respond_to do |format|
-      if @store.update(store_params)
-        format.html { redirect_to store_url(@store), notice: "Loja atualizada com sucesso." }
-        format.json { render :show, status: :ok, location: @store }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @store.errors, status: :unprocessable_entity }
-      end
+    if @store.update(store_params)
+      @store.image.attach(params[:store][:image]) if params[:store][:image].present?
+      render json: @store.as_json(methods: :image_url), status: :ok
+    else
+      render json: @store.errors, status: :unprocessable_entity
     end
   end
 
-
   def destroy
-    @store.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to stores_url, notice: "Loja excluída com sucesso." }
-      format.json { head :no_content }
+    if @store.soft_delete
+      respond_to do |format|
+        format.html { redirect_to stores_url, notice: "Loja excluída com sucesso." }
+        format.json { render json: { message: "Loja excluída com sucesso." }, status: :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to stores_url, alert: "Falha ao excluir loja." }
+        format.json { render json: { error: "Falha ao excluir loja." }, status: :unprocessable_entity }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_store
-      @store = Store.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def store_params
-      required = params.require(:store)
-      if current_user.admin?
-        required.permit(:name, :user_id)
-      else
-        required.permit(:name)
-      end
+  def set_store
+    @store = Store.find(params[:id])
+  end
+
+  def store_params
+    required = params.require(:store)
+    if current_user.admin?
+      required.permit(:name, :user_id)
+    else
+      required.permit(:name)
     end
+  end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,14 +1,25 @@
 class Store < ApplicationRecord
-    belongs_to :user
-    before_validation :ensure_seller
-    has_many :products
-  
-    validates :name, presence: true, length: { minimum: 3 }
-  
-    private
-  
-    def ensure_seller
-      self.user = nil if self.user && !self.user.seller?
-    end
+  belongs_to :user
+  before_validation :ensure_seller
+  has_many :products
+  has_one_attached :image
+  before_validation :ensure_seller
+  validates :name, presence: true, length: { minimum: 3 }
+
+  def soft_delete
+    update(deleted: true)
   end
-  
+
+  def undelete
+    update(deleted: false)
+  end
+
+   def image_url
+    Rails.application.routes.url_helpers.rails_blob_url(image, host: "localhost:3000") if image.attached?
+  end
+  private
+
+  def ensure_seller
+    self.user = nil if self.user && !self.user.seller?
+  end
+end
