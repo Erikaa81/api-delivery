7bfa52dfa005db7d319584428af64de52680cf93
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-06-14 11:24:43 -0300

    feat: Adiciona jobs em background

--- app/jobs/payment_job.rb
+++ app/jobs/payment_job.rb
@@ -1,23 +1,25 @@
-class PaymentJob
-    def perform(order:, value:, number:, valid:, cvv:)
-      params = { value: value, number: number, valid: valid, cvv: cvv }
-      response = con.post("/payments", params.to_json)
-      # order.paid if response.success?
-    end
+class PaymentJob < ApplicationJob
+  queue_as :default
+
+  def perform(order:, value:, number:, valid:, cvv:)
+  params = { value: value, number: number, valid: valid, cvv: cvv }
+  response = con.post("/payments", params.to_json)
+  # order.paid if response.success?
+  end
   
-    private
+  private
   
-    def config
-      Rails.configuration.payment
-    end
+  def config
+    Rails.configuration.payment
+  end
   
-    def con
-      @con ||= Faraday.new(
-        url: config.host,
-        headers: {
-          "Content-Type" => "application/json",
-          "Accept" => "application/json",
-        }
-      )
+  def con
+    @con ||= Faraday.new(
+      url: config.host,
+      headers: {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json",
+      }
+    )
     end
   end
